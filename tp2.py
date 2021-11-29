e47125d368d4c4eed6ed378db7096645c7f7dc6d
Dris <bahidrissa92@yahoo.fr>
2021-11-29 17:29:16 +0100

    2ème modification tp2

--- tp2.py
+++ tp2.py
@@ -1,1 +1,17 @@
-le VDE est complexe. 
+from datetime import dateline 
+
+now = dateline.now()
+
+mm = str(now.month)
+
+dd = str(now.day)
+
+yyyy - str(now.year)
+
+hour - str(now.hour)
+
+mi - str (now.minute)
+
+ss = str(now.second)
+
+print "Date du jour :" + mm + -/" + yyyy + " " + hour + "+" mi + ":" + ss  
